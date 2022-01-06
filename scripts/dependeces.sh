bfef3883196341f674f24e44075ddaa85fc6b60f
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2022-01-05 22:31:04 -0300

    [A] Sources

--- scripts/dependeces.sh
+++ scripts/dependeces.sh
@@ -1,2 +1,52 @@
-sudo apt update && sudo apt install upgrade
-sudo apt install zsh git gh python3 python3-pip nodejs man
+linux=(
+  gh
+  zsh
+  git
+  curl
+  nodejs
+  python3
+  python3-pip
+  python3-venv
+  brave-browser
+  apt-transport-https
+)
+
+termux=(
+  gh
+  man
+  zsh
+  git
+  curl
+  glow
+  nodejs
+  python3
+)
+
+check_command() {
+  dpkg -s ${1} &> /dev/null
+}
+
+start_install() {
+  apt update
+  apt install "${@}"
+}
+
+installer() {
+  for program in "${@}"
+  do
+    if ! check_command "${program}"
+    then
+      start_install "${@}"
+      exit
+    fi
+  done
+
+  echo "Already installed"
+}
+
+if [ -z "$USER" ]
+  then
+    installer ${termux[@]}
+  else
+    installer ${linux[@]}
+fi
---
+++ scripts/sources.sh
@@ -0,0 +1,36 @@
+install_source() {
+  sudo curl -fsSLo "${key}" "${url}/${gpg}"
+  echo "deb [arch=${architecture} signed-by=${key}] ${url} stable main" | sudo tee "${src}" > /dev/null
+}
+
+
+architecture=`dpkg --print-architecture`
+
+source_list='/etc/apt/sources.list.d'
+keyring='/usr/share/keyrings'
+
+
+# Github Cli
+
+url="https://cli.github.com/packages"
+
+gpg="githubcli-archive-keyring.gpg"
+list="github-cli.list"
+
+key="${keyrings}/${gpg}"
+src="${source_list}/${list}"
+
+install_source
+
+
+# Brave Browser
+
+url="https://brave-browser-apt-release.s3.brave.com"
+
+gpg="brave-browser-archive-keyring.gpg"
+list="brave-browser-release.list"
+
+key="${keyrings}/${gpg}"
+src="${source_list}/${list}"
+
+install_source
