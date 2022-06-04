d83728cd5341ff99a709e1a61cbb33a896e1823a
Termux <473829+termux@users.noreply.github.com>
2022-06-04 01:45:51 -0300

    refactor(nvim,script): update inital instalers

---
+++ scripts/nvim.sh
@@ -0,0 +1,14 @@
+# Dependência
+# python3 -m pip install neovim
+#
+# # Cria o diretório do vim-plug
+# mkdir -p ~/.local/share/nvim/site/autoload/
+#
+# # Instala o vim-plug
+# curl -sLo ~/.local/share/nvim/site/autoload/plug.vim \
+#   https://github.com/junegunn/vim-plug/raw/master/plug.vim \
+#   || exit 1
+
+# Inicia o neovim com um comando para instalar
+# todos os plugins e extensões
+nvim '+PlugInstall'
