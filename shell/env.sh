c7eac63a2f6fafc29abeae845285d8109a98ffcc
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-03-02 18:44:59 -0300

    refactor: configurando

--- shell/env.sh
+++ shell/env.sh
@@ -1,6 +1,15 @@
 export EDITOR=nvim
 
 export PATH="${PATH}:${HOME}/.local/bin"
+export PATH="${PATH}:${HOME}/.pyenv/bin"
+
+export FLYCTL_INSTALL="/home/brunodavi/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
 
 export NVM_DIR="${HOME}/.nvm"
+
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
