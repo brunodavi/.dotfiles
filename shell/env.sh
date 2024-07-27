b9f6047d3bb68bf333d0bcccd248c89ef076462a
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-07-27 12:18:01 -0300

    feat: env.sh mise activate

--- shell/env.sh
+++ shell/env.sh
@@ -1,28 +1,6 @@
 export EDITOR=nvim
 
 export PATH="${PATH}:${HOME}/.local/bin"
-export PATH="${PATH}:${HOME}/.pyenv/bin"
 
-export FLYCTL_INSTALL="/home/brunodavi/.fly"
-export PATH="$FLYCTL_INSTALL/bin:$PATH"
-
-export NVM_DIR="${HOME}/.nvm"
-
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-
-command -v pyenv >/dev/null && eval "$(pyenv init -)"
-
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-GCLOUD="$HOME/google-cloud-sdk/"
-
-# The next line updates PATH for the Google Cloud SDK.
-[ -f "$GCLOUD/path.zsh.inc" ] && . "$GCLOUD/path.zsh.inc"
-
-# The next line enables shell command completion for gcloud.
-[ -f "$GCLOUD/completion.zsh.inc" ] && . "$GCLOUD/completion.zsh.inc"
+shell=$(basename $SHELL)
+eval "$(mise activate $shell)"
