6c7f2ce7fd35a8263ae1f3bda73540dae5c61611
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-07-28 11:35:01 -0300

    feat: auto install dependencies

--- shell/env.sh
+++ shell/env.sh
@@ -1,6 +1,3 @@
 export EDITOR=nvim
 
 export PATH="${PATH}:${HOME}/.local/bin"
-
-shell=$(basename $SHELL)
-eval "$(mise activate $shell)"
--- shell/install-deps.sh
+++ shell/install-deps.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env sh
 
-# .termux
 if [ -z $USER ]
-then
+then # Termux
   pkg update
 
-  pkg install zsh eza bat neovim vim gh -y
-else
+  pkg install python zsh eza bat neovim -y
+
+else # Linux
   apt update
 
+  # Dependences
   sudo apt install zsh -y
   sudo apt install eza -y
-  sudo apt install bat -y
+
+  ## Neovim
+  sudo apt-get install software-properties-common -y
+  sudo apt-get install python-software-properties -y
+
+  sudo add-apt-repository ppa:neovim-ppa/stable
+  sudo apt-get update
+  sudo apt-get install neovim -y
 fi
