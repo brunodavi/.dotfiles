e5d6fcffab99fce125c281ba6ec55c57db33c836
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-07-29 16:29:51 -0300

    feat!: remove dependencies pc

--- shell/install-deps.sh
+++ shell/install-deps.sh
@@ -4,20 +4,7 @@ if [ -z $USER ]
 then # Termux
   pkg update
 
-  pkg install python zsh eza bat neovim -y
-
-else # Linux
-  apt update
-
-  # Dependences
-  sudo apt install zsh -y
-  sudo apt install eza -y
-
-  ## Neovim
-  sudo apt-get install software-properties-common -y
-  sudo apt-get install python-software-properties -y
-
-  sudo add-apt-repository ppa:neovim-ppa/stable
-  sudo apt-get update
-  sudo apt-get install neovim -y
+  pkg install python zsh eza neovim -y
+else # Linux is Manual
+  echo install python zsh eza and neovim
 fi
