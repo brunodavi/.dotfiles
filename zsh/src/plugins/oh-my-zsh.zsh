8c99da445908b3406cb87605d4efa74ef24fa90a
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-10-14 09:27:39 -0300

    feat: add sudo

--- zsh/src/plugins/oh-my-zsh.zsh
+++ zsh/src/plugins/oh-my-zsh.zsh
@@ -1,28 +1,16 @@
-# Aliases
-aliases
-
 # Plugins
+sudo
 magic-enter
-web-search
-term_tab
-bgnotify
-colorize
-dircycle
 extract
 
 # Completions
 compleat
 
 ## Git
-git
 gitfast
 gitignore
-git-extras
 
 ## Programms
 pip
 node
 httpie
-
-## Build
-
